81ef63882de8dd1adf7d6b2a3fecf228be5d7ae8
MladenEnev <45882115+MladenEnev@users.noreply.github.com>
2019-12-15 14:42:06 +0200

    Add files via upload

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -3,9 +3,14 @@ import React  from 'react'
 class Home extends React.Component{
     render(){
         return(
-            <div>
-                <h3>Home Page</h3>
-            </div>
+        <div className="row">
+            <div className="col-10 mx-auto my-2 text-center text-title">
+                <h1 className="text-capitalize font-weight-bold">
+                    <strong>Welcome to my car app</strong>
+                    <h3>We have some awesome second hand cars for you to choose from</h3>
+                </h1>
+            </div>   
+        </div>
         )
     }
 }
